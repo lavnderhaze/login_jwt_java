1a46e8a850085a491376e179fda7e5e61dfb2c14
Daniela <danielacb2013@gmail.com>
2025-03-28 21:29:44 -0500

    Correción

--- src/main/java/com/example/login/jwt/authController.java
+++ src/main/java/com/example/login/jwt/authController.java
@@ -3,12 +3,14 @@ package com.example.login.jwt;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class authController {
     
     @Autowired
